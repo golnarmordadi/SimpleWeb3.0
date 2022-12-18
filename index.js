8fca3f89af8e99efd2f9911a136334d5df3de206
Hakimeh Mordadi <golnarmordadi@gmail.com>
2022-12-18 22:48:15 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const Web3 = require('web3');
+
+const web3 = new Web3('http://127.0.0.1:8545');
+
+
+/** This will give you list of accounts that we got with using gancha-cli */
+web3.eth.getAccounts().
+then(console.log);
+
+/** This will give the block number */
+web3.eth.getBlockNumber().
+then(console.log);
 
